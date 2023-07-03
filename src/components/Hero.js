8e3dadf43716449d834da71d03d1c4ffb7750951
Dwyan Alford <dwyan.alford@gmail.com>
2023-07-03 01:58:18 -0700

    started adding transitions to Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,20 @@
 //components/Hero.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import heroData from '@/data/heroData';
 
 const Hero = ({ pageName }) => {
   const pageData = heroData.find((data) => data.page === pageName);
 
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
     pageData.content.map((data, index) => (
       <div key={index} 
-           className="hero flex w-full h-screen p-4 relative"
+           className={`hero flex w-full h-screen p-4 relative ${loaded ? 'fade-in' : ''}`}
            style={{ backgroundImage: `url(${data.image.large.url})` }}>
           <div className="hero-item absolute bottom-40 left-0 p-8 md:p-0 md:h-[500px] md:static md:ml-[60px] md:mt-[90px]">
             <h2
@@ -32,3 +38,4 @@ const Hero = ({ pageName }) => {
 };
 
 export default Hero;
+
