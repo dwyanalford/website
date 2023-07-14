74cbc62eb2ee6e8cd16df816cf5a140a831a65a9
Dwyan Alford <dwyan.alford@gmail.com>
2023-07-13 23:48:49 -0700

    completed experience template component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,6 @@
 // components/Experience.js
-import styles from '@/styles/Experience.module.css'
 import experienceData from '@/data/experienceData';
+import Image from 'next/image';
 
 const Experience = () => {
   // Sort experience data by workDates in descending order (most recent job first)
@@ -13,29 +13,30 @@ const Experience = () => {
   return (
     <>
       {sortedExperienceData.map((exp, index) => (
-        <div key={index} className={styles.wrapper}>
-          <div className={`${styles.row} borderDiv`}>
-          <div className={`${styles.column} ${styles.leftColumn}`}>
-          <h1>{exp.title}</h1>
-          <h2>Company: {exp.company}</h2>
-          <h4>{exp.workDates}</h4>
-          <h4>Type of work:{exp.workType}</h4>
-          <h3>Tasks:</h3>
-          <h4>Type of work:&nbsp;{exp.workType}</h4>
-          <ul>
-            {exp.tasks.map((task, index) => (
-              <li key={index}>{task}</li>
-            ))}
-          </ul>
-        </div>
-            <div className={`${styles.column} ${styles.rightColumn}`}>
-              <img
+        <div key={index} className="content">
+          <div className="content-container lg:gap-2">
+            <div className="w-full bg-center bg-cover pl-3 md:flex-1 lg:p-4">
+              <h1 className='font-bold text-2xl pb-2 pt-6 border-t border-blue-300'>{exp.title}</h1>
+              <h2 className='font-semibold text-xl pb-2'>Company: {exp.company}</h2>
+              <h4 className='text-lg pb-4'>{exp.workDates}</h4>
+              <h4 className='pb-6'>Type of work:&nbsp;{exp.workType}</h4>
+              <h3 className='pb-3 pt-2'>Tasks:</h3>
+              <ul>
+                {exp.tasks.map((task, index) => (
+                  <li key={index} className='mb-3'>{task}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="w-full md:flex-1">
+              <Image
                 src={exp.imgSrc}
                 alt={exp.alt}
-                className={styles.backgroundImage}
+                className='mx-auto w-full pt-3'
+                width={400} 
+                height={100}
               />
-              <div className={styles.buttonContainer}>
-                <a href={exp.website} className={`button-blue ${styles.centered}`} target="_blank" rel="noopener noreferrer">
+              <div className=''>
+                <a href={exp.website} className='' target="_blank" rel="noopener noreferrer">
                   {exp.websiteTitle}
                 </a>
               </div>
