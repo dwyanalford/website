0d3a40cdb451d5e1d1699e0b481eb08f9ad38f2a
Dwyan Alford <dwyan.alford@gmail.com>
2023-07-22 00:26:24 -0700

    design/layout of education page

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -4,13 +4,15 @@ const educationData = [
       school: "Queen's College, Taunton",
       startDate: 'Sept 1984',
       endDate: 'July 1991',
-      title: 'Advanced Levels & GCSEs',
-      details: 'Economics, History and Politics',
-      type: 'Advanced Levels',
+      title: 'Advanced Levels / GCSEs',
+      details: "Studied Economics, History and Politics for Advanced Levels. At Queen’s College they provide students with a well-rounded education for life. The school was founded in 1843. Christian Methodist foundation.",
+      type: 'Senior Secondary School',
       location: 'Somerset, England',
-      website: '',
+      website: 'https://www.queenscollege.org.uk/',
+      short: "Queen's College, Taunton",
       images: {
-        school: '/images/queens-logo.jpg',
+        logo: '/images/queens-logo.jpg',
+        schoolBackground: '/images/education-queens-background.jpg',
         showcase: '/images/education-queens.jpg'
       }
     },
@@ -19,40 +21,75 @@ const educationData = [
       startDate: 'Feb 2017',
       endDate: 'Aug 2017',
       title: 'Full Stack Web Development',
-      details: 'React.js, MongoDB, Node.js, Express.js',
-      type: 'Certification',
-      location: 'Arlington, Virginia',
-      website: '',
+      details: '24 Weeks covering full stack web development, including HTML5, CSS3, JavaScript, jQuery, Express.js, React.js, Node.js, progressive web apps, agile methodology, computer science, database theory, MongoDB, MySQL and Git',
+      type: 'GW Coding Boot Camp',
+      location: 'Arlington Campus, Virginia, USA',
+      website: 'https://bootcamp.cps.gwu.edu/coding/curriculum/',
+      short: 'GWU Curriculum',
       images: {
-        school: '/images/gwuLogo.png',
+        logo: '/images/gwuLogo.png',
+        schoolBackground: '/images/education-gwu-background.jpg',
         showcase: '/images/education-gwu.jpg'
       }
     },
     {
-      school: 'Revature',
-      startDate: 'Oct 2022',
-      endDate: 'Feb 2023',
-      title: 'Test Automation Engineer',
-      details: 'Java backend with Selenium & JUnit for automating the testing of web apps',
-      type: 'On the Job Training',
-      location: '100% Remote',
-      website: '',
+      school: 'Ghana International School',
+      startDate: 'July 1983',
+      endDate: 'Feb 1985',
+      details: 'English National Curriculum and Cambridge International Examinations. Ghana International School (GIS) was founded on 1st September 1955 to provide international education to students of all races and creeds, at a time when Ghana was yet to achieve full independence, and international schools were a rarity.',
+      type: 'Primary',
+      location: 'Accra, Ghana',
+      website: 'https://gis.edu.gh/',
+      short: 'Ghana International School Online',
       images: {
-        school: '/images/revature.jpg',
-        showcase: '/images/education-revature.jpg'
+        logo: '/images/ghanaLogo.jpg',
+        schoolBackground: '/images/education-ghana-background.jpg',
+        showcase: '/images/education-ghana.jpg'
       }
     },
     {
-        school: 'New York University',
-        startDate: ' Jan 1992',
+      school: 'Kenton College Preparatory School',
+      startDate: 'July 1981',
+      endDate: 'Jun 1983',
+      details: 'British Curriculum, ages of 6 and 13 years, Located in the leafy suburb of Kileleshwa in Nairobi, pupils at Kenton College Preparatory School enjoy 35 acres of excellent facilities and an outstanding education in a friendly and welcoming international school community.',
+      type: 'Preparatory School',
+      location: 'Nairobi, Kenya',
+      website: 'https://www.kentonschoolnairobi.com/',
+      short: 'Kenton College Online',
+      images: {
+        logo: '/images/kentonLogo.png',
+        schoolBackground: '/images/education-kenton-background.jpg',
+        showcase: '/images/education-kenton.jpg'
+      }
+    },
+    {
+      school: 'School of the Blessed Sacrament',
+      startDate: 'April 1975',
+      endDate: 'May 1978',
+      details: 'Catholic education on the west side of Manhattan, founded in 1903. Integrates a traditional educational program with a focus on STREAM (science, technology, engineering, religion, arts, music) in order to provide a well-rounded learning experience for the whole child.',
+      type: 'Primary',
+      location: '456 W 52nd Street, New York City, USA',
+      website: 'https://www.sblsnyc.org/',
+      short: 'School of the Blessed Sacrament Online',
+      images: {
+        logo: '/images/blessLogo.png',
+        schoolBackground: '/images/education-bless-background.jpg',
+        showcase: '/images/education-bless.jpg'
+      }
+    },
+    {
+        school: 'New York University, Stern School of Business',
+        startDate: 'Jan 1992',
         endDate: 'June 1996',
-        title: 'Accounting & Marketing',
-        details: 'Double Major in Accounting & Marketing',
-        type: 'Bachelor of Science Degree',
-        location: 'New York, NY',
-        website: '',
+        title: 'BS Accounting & Marketing',
+        details: "Accounting program is ranked in the top 10 in the U.S. By combining courses in business fundamentals with a broad-based liberal arts foundation, future business leaders are given the skills, expertise, and intellectual sophistication needed to advance in today's dynamic business environment.",
+        type: 'Bachelor of Science',
+        location: 'New York, USA',
+        website: 'https://www.stern.nyu.edu/programs-admissions/undergraduate/academics/bs-degree-business-ms-accounting',
+        short: 'NYU Stern Schhol of Business',
         images: {
-          school: '/images/nyu-logo.jpg',
+          logo: '/images/nyu-logo.jpg',
+          schoolBackground: '/images/education-nyu-background.jpg',
           showcase: '/images/education-nyu.jpg'
         }
       },
