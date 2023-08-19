c907f87725b20744db8ea0cc735ed2a3a41dd076
Dwyan Alford <dwyan.alford@gmail.com>
2023-08-19 00:34:15 -0700

    redesigned content and layout for projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,9 +9,9 @@ const Projects = () => {
     <>
       {projectsData.map((project, index) => (
         <section key={index} className="flex w-full mb-5 xl:p-6 lg:mt-9">
-          <div className="projects-container flex flex-wrap w-full gap-3 p-2 lg:mb-7 sm:shadow-lg hover:shadow-xl">
-            <div className='projects-item-1 lg:p-6 lg:flex-1 mx-auto'>
-              <div className='projects-summary lg:p-6 mx-auto max-w-2xl'>
+          <div className="projects-container flex flex-wrap w-full gap-3 p-2 lg:pb-12 sm:shadow-lg hover:shadow-xl">
+            <div className='projects-item-1 lg:p-6 lg:flex-1 mx-auto rounded-xl'>
+              <div className='projects-summary mx-auto max-w-2xl pl-4 custom-shadow-left-white'>
                 <h1 className='font-bold p-3 text-2xl'>
                   {project.title}
                   <span className='grey-highlight text-base ml-4'>
@@ -34,10 +34,10 @@ const Projects = () => {
               </div>
             </div>
 
-            <div className="projects-item-2 lg:p-4 w-full lg:flex-1">
-              <div className='p-3'>
+            <div className="projects-item-2 w-full lg:flex-1 rounded-xl">
+              <div className='pro-container rounded-xl mx-auto max-w-2xl pt-5'>
                 {project.videoId ? (
-                  <VideoPlayer videoSrc={project.videoId} caption={project.caption} />
+                  <VideoPlayer videoId={project.videoId} caption={project.caption} />
                 ) : (
                   <Image
                     src={project.backupImage}
@@ -46,17 +46,40 @@ const Projects = () => {
                     width={560} 
                     height={315}
                   />
+
+                  
                   
                 )}
-
-                <h3 className='p-2 text-center pt-8 pb-5'>Technologies Used:</h3>
-                <h4 className='p-2 text-center pb-5 text-xl font-semibold'>{project.technologies}</h4>
-                <div className='p-2 text-center pt-5 pb-8'>
-                  <a className='button blue-button' href={project.link}>VIEW CODE BASE ON GITHUB</a>
+                
+                  
+                  <div className='technologies-bg p-2 rounded-xl custom-shadow-top-white max-w-xl mx-auto mt-6'> 
+                  <p className='pt-6 font-semibold max-w-lg mx-auto rounded-sm pb-6'>Video: {project.caption}</p>
+                    <h3 className='p-2 pl-5 pt-10'>Technologies Used:</h3>
+                    <h4 className='pl-5 text-xl font-semibold'>{project.technologies}</h4>
+                    <div className='p-2 text-center pt-5 pb-5 mt-4'>
+                      <a className='button blue-button' href={project.link}>VIEW CODE BASE ON GITHUB</a>
+                    </div>
                 </div>
               </div>
             </div>
             <style jsx>{`
+            .projects-item-2 {
+              // background-color: #6d7272;
+              
+            }
+            .pro-container {
+              // background-color: #c9d1d3;
+              // background-color: #b7bdbf;
+              // background-color: #000;
+            }
+
+            .pro-container:hover {
+              // background-color: #b7bdbf;
+            }
+            .technologies-bg {
+              // background-color: #ffffff;
+              background-color: #c9d1d3;
+            }           
                 
             `}</style> 
           </div>
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -2,25 +2,22 @@
 
 function VideoPlayer({ videoId, caption }) {
   return (
-    <div className="">
-      <iframe 
-        width="560" 
-        height="315" 
-        src={`https://www.youtube.com/embed/${videoId}`} 
-        alt={caption}
+    <div className="video-container max-w-xl mx-auto md:rounded-lg">
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${videoId}?controls=0`}
         title={caption}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         className="video-player mx-auto"
-    ></iframe>
+      ></iframe>
 
-
-      <p className='text-center pt-4'>{caption}</p>
       <style jsx>{`
       @media screen and (max-width: 560px) {
         .video-player {
           width: 400px;
           height: 244px;
-        }     
+        }  
       }
         
       `}</style> 
